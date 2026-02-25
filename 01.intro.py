fcbb313b8f3d55ed60d826bcfcb1eaea37dfee81
Silviu Ojog <sil.ojog@gmail.com>
2026-02-25 10:59:55 +0200

    Intro Python

---
+++ 01.intro.py
@@ -0,0 +1,22 @@
+
+# Acesta este un comentariu
+print("hello world!!!")
+
+print('hello world!!!'); # ";" nu este obligatorie, se doreste sa nu se puna
+
+print("""hello world!!!""")
+
+
+print('''hello world!!!''')
+
+print('''hello world!!!''')
+
+print('''hello world!!!''')
+
+print('''hello world!!!''')
+
+print('''hello world!!!''')
+print('''hello world!!!''')
+
+x = 2
+print(x)
---
+++ 03.conditii.py
@@ -0,0 +1,31 @@
+
+if (10 > 3):{
+    print("Adevarat")
+}
+
+if (10 > 3):{
+print("Adevarat")
+}
+    
+if (10 > 3):{print("Adevarat")
+}
+    
+# Recomandare
+if 10 > 3:
+    print("Adevarat")
+
+
+# Nerecomandat
+if 10 > 3:
+ print("Adevarat")
+
+# Nerecomandat
+if 10 > 3:
+            print("Adevarat")
+
+
+
+
+x = "salut"
+if x:
+    print("x nu este gol")
